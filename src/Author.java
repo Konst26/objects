91ff4b3c99e32abfc84c94a7a7ec7ac18daa695a
Константин <osokinkonstantin@mail.ru>
2024-05-26 22:19:31 +0300

    домашнее задание

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String firstName;
+    private String lastNAme;
+
+    public Author(String firstName, String lastNAme) {
+        this.firstName = firstName;
+        this.lastNAme = lastNAme;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastNAme() {
+        return lastNAme;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        Author mihail = new Author("Mihail", "Lermontov");
+        Book road = new Book("Road", mihail, 2000);
+        System.out.println(road.getTitle() + " " + road.getPublisherYear());
+        road.setPublisherYear(2005);
+        System.out.println(road.getTitle() + " " + road.getPublisherYear());
+        System.out.println(road.getAuthor().getFirstName() + " " + road.getAuthor().getLastNAme());
+
+    }
+}
