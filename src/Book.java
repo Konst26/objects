e1d35242596d9f680b2d41e767b57fe53147c2b4
Константин <osokinkonstantin@mail.ru>
2024-05-28 21:59:00 +0300

    домашнее задание 2

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String title;
     private Author author;
@@ -24,4 +26,27 @@ public class Book {
     public void setPublicationYear(int publicationYear) {
         this.PublicationYear = publicationYear;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author=" + author +
+                ", PublicationYear=" + PublicationYear +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return PublicationYear == book.PublicationYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, PublicationYear);
+    }
 }
+
